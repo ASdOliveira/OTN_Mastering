a71fa0e24e8fd5d70ebd817e3ad5a1fd4fa038cd
ASdOliveira <arysson.silva@hotmail.com>
2021-05-17 20:34:22 -0300

    adding opticalLink class

---
+++ base/OpticalLink.py
@@ -0,0 +1,16 @@
+class OpticalLink:
+    __id = 0
+    # __fiberType missingType
+    __length = 0.0
+    __attenuationCoefficient = 0.0
+    __pmdCoefficient = 0.0
+    __sharedGroupRisk = ""
+    __regions = []
+    # __mode missingType
+    __zeroDispersionWaveLength = 0.0
+    __sourceSite = 0
+    __destSite = 0
+    # __channels missingType
+    # __application missingType
+    __outOfOperation = False
+    __dispersionAt1550Nm = 0.0
