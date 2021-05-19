c4e06509e465ae80349d99a3a7bc55c4b66ac5f0
ASdOliveira <arysson.silva@hotmail.com>
2021-05-19 20:09:57 -0300

    Adding siteClass and improving Ots class

--- base/Ots.py
+++ base/Ots.py
@@ -1,4 +1,4 @@
 class Ots:
     def __init__(self):
         self.id = 0
-        self.interfaces = [] #check to add the InterfacesClass
+        self.interfacesList = [] #check to add the InterfacesClass
