602041d80e4e609ae9bc8857f43232df135ddd92
ASdOliveira <arysson.silva@hotmail.com>
2021-06-16 15:34:57 -0300

    Improving the way to load Network topology

--- Utils/File.py
+++ Utils/File.py
@@ -4,13 +4,26 @@ All Input data (Network modeling) should be stored in Input folder and
 for the output data will be stored at Output folder
 """
 from pathlib import Path
+import csv
+import networkx as nx
 
 
-def importGraphFromFile(fileName):
-    # myPath = os.path.abspath(os.path.dirname(__file__))
-    # path = os.path.join(myPath, "..", "Input", str(fileName))
+def importNetworkTopologyToGraph(fileName):
+    G = nx.MultiGraph()
+
     relativePath = "../Input/" + str(fileName)
     path = Path(__file__).parent / relativePath
-    with path.open() as f:
-        f = open(path, "r")
+
+    with open(path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                print(row[0], row[1])
+                G.add_edge(row[0], row[1])
+                line_count += 1
+    return G
+
 
--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 
-from Utils.File import importGraphFromFile
-# 1: Load data
+from Utils.File import *
+import networkx as nx
+import matplotlib.pyplot as plt
 
+# 1: Load the Network Topology
 
-importGraphFromFile("test.txt")
-# 2: Convert data loaded to code
-# 3: Calculate the fails Scenario
-# 4: Runs the Multi-Objective Algorithm
-# 4.1: Evaluate the interfaces Quantity
-# 4.2: Evaluate the TIFF
-# 5: Get results
+G = importNetworkTopologyToGraph("Topologia1.csv")
+print(G.nodes())
+print(G.edges())
+
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos, node_color = 'r', node_size = 100, alpha = 1)
+ax = plt.gca()
+for e in G.edges:
+    ax.annotate("",
+                xy=pos[e[0]], xycoords='data',
+                xytext=pos[e[1]], textcoords='data',
+                arrowprops=dict(arrowstyle="->", color="0.5",
+                                shrinkA=5, shrinkB=5,
+                                patchA=None, patchB=None,
+                                connectionstyle="arc3,rad=rrr".replace('rrr',str(0.3*e[2])
+                                ),
+                                ),
+                )
+plt.axis('off')
+plt.show()
+
+# 2: Load the service (demand)
+# 3: Model the data
+# 4: Calculate the fails Scenario
+# 5: Runs the Multi-Objective Algorithm
+# 5.1: Evaluate the interfaces Quantity
+# 5.2: Evaluate the TIFF
+# 6: Get results
