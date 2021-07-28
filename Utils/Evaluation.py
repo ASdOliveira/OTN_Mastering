0ad82c38d67eee74e4b443e61d29bd39a27c7bb7
ASdOliveira <arysson.silva@hotmail.com>
2021-07-28 20:00:43 -0300

    Adding a function to calculate the shortest path

--- Utils/Evaluation.py
+++ Utils/Evaluation.py
@@ -2,6 +2,7 @@ import copy
 from copy import deepcopy
 import networkx as nx
 from Models.TELink import TELink
+from Utils.ServiceEnum import ServiceEnum
 
 
 def evaluateNetwork(network, chromosome):
@@ -34,14 +35,18 @@ def _calculateTIRF(network, chromosome):
 
     # At This point the Network Graph has the Network modeled on TELink rather than Link bundles
     _convertToGraph(chromosome, TELinks, NetworkGraph)
-    NetworkGraphCopy = copy.deepcopy(NetworkGraph)
 
-    IsAllServicesAllocated = _allocateServices(NetworkCopy, NetworkGraph, NetworkGraphCopy)
+    IsAllServicesAllocated = _allocateServices(NetworkCopy, NetworkGraph)
 
     if not IsAllServicesAllocated:
         # TODO: Implement a penalty
         pass
 
+   # IsProtectionRoutesAllocated = _allocateProtection()
+
+    # if not IsProtectionRoutesAllocated:
+    #     # TODO: Implement a penalty
+    #     pass
     return None
 
 
@@ -55,17 +60,16 @@ def _convertToGraph(chromosome, TELinks, NetworkGraph):
         count += 1
 
 
-def _allocateServices(NetworkCopy, NetworkGraph, NetworkGraphCopy):
+def _allocateServices(NetworkCopy, NetworkGraph):
     NonAllocatedServices = []
     IsServicesAllocated = True
+    NetworkGraphCopy = copy.deepcopy(NetworkGraph)
 
     for service in NetworkCopy.Services:
-        all_edge_paths = nx.all_simple_edge_paths(NetworkGraphCopy, service.NodeFrom, service.NodeTo)
-        sorted_all_edge_paths = sorted(list(all_edge_paths), key=lambda a: len(a))
+        edges = _getShortestPathInMultigraph(NetworkGraphCopy, service.NodeFrom, service.NodeTo)
 
         AllocationIsCompleted = False
 
-        edges = sorted_all_edge_paths[0]
         for edge in edges:
             TELINK = (NetworkGraph.get_edge_data(edge[0], edge[1])[edge[2]]).get("link")
             TELINK.IsBusy = True
@@ -79,3 +83,21 @@ def _allocateServices(NetworkCopy, NetworkGraph, NetworkGraphCopy):
         IsServicesAllocated = False
 
     return IsServicesAllocated
+
+
+def _allocateProtection(NetworkCopy, NetworkGraph):
+    NetworkGraphCopy = copy.deepcopy(NetworkGraph)
+
+    for service in NetworkCopy.Services:
+        if service == ServiceEnum.MAIN_ROUTE_AND_BACKUP or ServiceEnum.MAIN_ROUTE_AND_BACKUP_AND_RESTORATION:
+            # TODO: Allocate the backup route.
+            # Remember: backup route must be a disjoint route, maybe the main route should be saved
+            pass
+    return False
+
+
+def _getShortestPathInMultigraph(G, Source, Target):
+    all_edge_paths = nx.all_simple_edge_paths(G, Source, Target)
+    sorted_all_edge_paths = sorted(list(all_edge_paths), key=lambda a: len(a))
+    edges = sorted_all_edge_paths[0]
+    return edges
