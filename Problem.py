d78a057a7b60df7457a345f944787c610fd22bca
ASdOliveira <arysson.silva@hotmail.com>
2021-08-09 21:40:20 -0300

    First integration of Framework and problem

---
+++ Problem.py
@@ -0,0 +1,31 @@
+from jmetal.core.problem import IntegerProblem
+from jmetal.core.solution import IntegerSolution
+from Utils import Evaluation
+
+
+class OTNProblem(IntegerProblem):
+    def __init__(self, network, number_of_variables):
+        super(OTNProblem, self).__init__()
+        self.number_of_objectives = 2
+        self.number_of_constraints = 0
+        self.number_of_variables = number_of_variables
+
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ['InterfacesQuantities', 'TIRF']
+
+        self.lower_bound = number_of_variables * [0]
+        self.upper_bound = number_of_variables * [8]
+
+        self.network = network
+
+    def evaluate(self, solution: IntegerSolution) -> IntegerSolution:
+        EvaluationResult = Evaluation.evaluateNetwork(self.network, solution.variables)
+        solution.objectives[0] = EvaluationResult[0]
+        solution.objectives[1] = EvaluationResult[1]
+        # solution.objectives[0] = solution.variables[0]
+        # solution.objectives[1] = h * g
+
+        return solution
+
+    def get_name(self):
+        return 'OTNProblem'
---
+++ teste.py
@@ -0,0 +1,36 @@
+from jmetal.algorithm.multiobjective.nsgaii import NSGAII
+from jmetal.operator import SBXCrossover, PolynomialMutation, IntegerPolynomialMutation
+#from jmetal.problem import ZDT1
+from jmetal.operator.crossover import IntegerSBXCrossover
+from jmetal.util.observer import ProgressBarObserver, VisualizerObserver
+from jmetal.util.termination_criterion import StoppingByEvaluations
+from jmetal.lab.visualization.plotting import Plot
+from jmetal.util.solution import get_non_dominated_solutions
+
+from Models.Network import Network
+from Problem import OTNProblem
+
+Net = Network(folderName="Topologia1")
+problem = OTNProblem(Net, len(Net.LinkBundles))
+
+max_evaluations = 2
+
+algorithm = NSGAII(
+    problem=problem,
+    population_size=100,
+    offspring_population_size=100,
+    mutation=IntegerPolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=0.20),
+    crossover=IntegerSBXCrossover(probability=1.0, distribution_index=20),
+    termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
+)
+
+
+algorithm.run()
+solutions = algorithm.get_result()
+
+
+
+front = get_non_dominated_solutions(solutions)
+
+plot_front = Plot(title='Pareto front approximation', axis_labels=['x', 'y'])
+plot_front.plot(front, label='NSGAII-ZDT1')
