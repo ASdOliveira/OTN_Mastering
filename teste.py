2dcd8c96dfc9a5802608fde020a830a43b8d90a3
ASdOliveira <arysson.silva@hotmail.com>
2021-08-16 18:50:07 -0300

    adding progress bar

--- teste.py
+++ teste.py
@@ -13,24 +13,27 @@ from Problem import OTNProblem
 Net = Network(folderName="Topologia1")
 problem = OTNProblem(Net, len(Net.LinkBundles))
 
-max_evaluations = 2
+max_evaluations = 200
 
 algorithm = NSGAII(
     problem=problem,
-    population_size=100,
-    offspring_population_size=100,
+    population_size=40,
+    offspring_population_size=40,
     mutation=IntegerPolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=0.20),
     crossover=IntegerSBXCrossover(probability=1.0, distribution_index=20),
     termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
 )
 
+progress_bar = ProgressBarObserver(max=max_evaluations)
+algorithm.observable.register(progress_bar)
 
 algorithm.run()
 solutions = algorithm.get_result()
 
-
-
 front = get_non_dominated_solutions(solutions)
 
-plot_front = Plot(title='Pareto front approximation', axis_labels=['x', 'y'])
-plot_front.plot(front, label='NSGAII-ZDT1')
+for f in front:
+    print(f.objectives[1])
+
+plot_front = Plot(title='Pareto front approximation', axis_labels=['Interfaces', 'TIRF'])
+plot_front.plot(front, label='NSGAII-OTN')
